426b59aec32e67a79756d0386dad547b8632263e
Nick Doiron <ndoiron@mapmeld.com>
2014-02-22 03:53:41 -0600

    add physical:true / physical:false tag and use OSM ids

--- data/refresh.py
+++ data/refresh.py
@@ -21,5 +21,35 @@ with open(scriptdir + '/coins.js', 'w') as f:
 for name, parser in parsers.iteritems():
 	for coin in coins:
 		coin = coin.lower()
+
+		# compare to existing points
+		existing_json = open(scriptdir + ('/data-%s-%s.json' % (name, coin)))
+		existing_pts = {}
+		for pt in json.load(existing_json):
+			existing_pts[pt["id"]] = pt
+
+		# fetch points from OSM
 		pts = parser.get_points(coin)
-		json.dump(pts, open(scriptdir + '/data-%s-%s.json' % (name, coin), 'w'), separators = (',', ':'))
+
+		# decide how to update map
+		for pt in pts:
+			if pt["id"] in existing_pts:
+				# update an existing point with latest data
+				# do not change physical status
+				pt["physical"] = existing_pts[pt["id"]]["physical"]
+				existing_pts[pt["id"]] = pt
+			else:
+				# new point from OSM
+				print("New OSM point:")
+				print(json.dumps(pt))
+				physical = raw_input("Adding point unless you respond N, n, or No here: ")
+				if physical.lower().replace(' ','').replace('no','n') == "n":
+					# don't map it for now
+					# can always be changed in the JSON
+					pt["physical"] = False
+				else:
+					# looks good!
+					pt["physical"] = True
+				existing_pts[pt["id"]] = pt
+
+		json.dump(existing_pts.values(), open(scriptdir + '/data-%s-%s.json' % (name, coin), 'w'), indent=2, sort_keys=True, separators = (',', ':'))
